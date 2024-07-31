d0df1100aab689167f0a5c05206bb62228b50f2c
WUTTHMONEKHIN <wutthmonekhin4@gmail.com>
2024-07-31 22:44:23 +0630

    design update

--- resources/views/FE/login.blade.php
+++ resources/views/FE/login.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Kapella Bootstrap Admin Dashboard Template</title>
+    <title>Oleez :: Blog</title>
     <!-- base:css -->
     <link rel="stylesheet" href="{{ asset('assets/BK/vendors/mdi/css/materialdesignicons.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/BK/vendors/base/vendor.bundle.base.css') }}">
@@ -17,7 +17,36 @@
     <!-- endinject -->
     <link rel="shortcut icon" href="{{ asset('assets/BK/images/favicon.png') }}" />
     <link rel="stylesheet" href="{{ asset('datatable/toastify.min.css') }}">
+    <style>
+        .form {
+            background: #000;
+            color: #ffffff;
+        }
 
+        .auth form .form-group .form-control,
+        .auth form .form-group .typeahead,
+        .auth form .form-group .tt-query,
+        .auth form .form-group .tt-hint {
+            background: transparent;
+            border-radius: 0;
+            font-size: .9375rem;
+            color: white;
+        }
+
+        .btn-submit {
+            padding: 17px 50px;
+            background-color: #ffc107;
+            border-radius: 0;
+            font-size: 18px;
+            font-weight: 700;
+        }
+
+        .btn-submit:hover {
+            background-color: transparent;
+            border: 1px solid #ffc107;
+            color: #fff !important;
+        }
+    </style>
 </head>
 
 <body>
@@ -27,10 +56,10 @@
                 <div class="content-wrapper d-flex align-items-center auth px-0">
                     <div class="row w-100 mx-0">
                         <div class="col-lg-4 mx-auto">
-                            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                            <div class="form text-left py-5 px-4 px-sm-5">
                                 <div class="brand-logo">
                                     <a href="{{ url('/') }}"><img
-                                            src="{{ asset('assets/FE/assets/images/Logo_2.svg') }}" alt="logo"></a>
+                                            src="{{ asset('assets/FE/assets/images/Logo_1.svg') }}" alt="logo"></a>
                                 </div>
                                 <h4>Hello! let's get started</h4>
                                 <h6 class="font-weight-light">Sign in to continue.</h6>
@@ -46,7 +75,7 @@
                                     </div>
                                     <div class="mt-3 text-center">
                                         <button type="submit"
-                                            class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn"
+                                            class="btn btn-block btn-submit text-black btn-lg font-weight-medium auth-form-btn"
                                             href="#">SIGN IN</button>
                                     </div>
                                     <div class="text-center mt-4 font-weight-light">
